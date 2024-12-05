80a19b25f157af80e72148f514d9ef1cc3e79233
권찬호 <rnjscksdud17@gmail.com>
2024-12-05 15:48:31 +0900

    🚀 다이얼로그 완성

--- G24W14WPFDialog/GundamDlg.xaml.cs
+++ G24W14WPFDialog/GundamDlg.xaml.cs
@@ -22,6 +22,67 @@ namespace G24W14WPFDialog
         public GundamDlg()
         {
             InitializeComponent();
+
+            Name2.Focus();
         }
+
+        public string MSName
+        {
+            get { return Name2.Text; }
+        }
+
+        public string MSModel => Model.Text;
+
+        public string MSParty => Party.Text;
+
+        private void OnOk(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(MSName))
+            {
+                MessageBox.Show(
+                    "이름을 입력하십시오.", 
+                    "입력 부족",
+                    MessageBoxButton.OK, 
+                    MessageBoxImage.Warning);
+
+                Name2.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(MSModel))
+            {
+                MessageBox.Show(
+                    "모델을 입력하십시오.",
+                    "입력 부족",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+
+                Model.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(MSParty))
+            {
+                MessageBox.Show(
+                    "소속을 입력하십시오.",
+                    "입력 부족",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+
+                Party.Focus();
+                return;
+            }
+
+            DialogResult = true;
+        }
+
+    
+
+        private void OnCancel(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+          
+
     }
 }
--- G24W14WPFDialog/MainWindow.xaml.cs
+++ G24W14WPFDialog/MainWindow.xaml.cs
@@ -24,7 +24,20 @@ namespace G24W14WPFDialog
         private void OnAdd(object sender, RoutedEventArgs e)
         {
             GundamDlg dialog = new GundamDlg();
-            dialog.ShowDialog();
+            if(dialog.ShowDialog() != true)
+                return;
+
+            Result.Text += $"{dialog.MSParty}의" + $"{dialog.MSModel} "
+                + $"{dialog.MSName}{(HasJongsung(dialog.MSName) ? "이" : "가")} "
+                + "추가되었습니다.\n";
+        }
+
+        public bool HasJongsung(string str)
+        {
+            if (str.Length < 1)
+                return true;
+            char last = str[str.Length - 1];
+            return (last - 44032) % 28 != 0 ? true : false;
         }
     }
 }
